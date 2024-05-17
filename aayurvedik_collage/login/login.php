0bde24ed0c1324948aa3d11e35e97b89bb808d82
purucrqzy <garadpurushottam38@gmail.com>
2024-05-18 01:01:08 +0530

    corrected the logo link problem in login php file

--- aayurvedik_collage/login/login.php
+++ aayurvedik_collage/login/login.php
@@ -79,29 +79,32 @@ if (isset($_GET['action']) && $_GET['action'] == 'logout') {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Online Pay Fees - GGU</title>
-  <link rel="shortcut icon" src="../image/favicon.ico" type="image/svg+xml">
+  <title>Students Login - SYCET</title>
+  <link rel="shortcut icon" href="../image/favicon.ico" type="image/svg+xml">
   <link rel="stylesheet" href="../css/login.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 </head>
 <body>
   <section class="header">
-    <nav>
-      <a href="index.html"><img src="../image/favicon.ico" id="logo-img"></a>
-      <div class="nav-links" id="navLinks">
-        <span class="icon" onclick="hidemenu()">&#10005;</span>
-        <ul>
-            <li><a href="../index.html">Home</a></li>
-            <li><a href="../Admission_page.php">Admission</a></li>
-            <li><a href="../hospital_page.html">Hospital</a></li>
-            <li><a href="../Contact_page.html">Contact</a></li>
-            <li><a href="../index.html#course_call">Course</a></li>
-            <li><a href="login/login.php">Login</a></li>
-        </ul>
-      </div>
-      <span class="icon" onclick="showmenu()">&#9776;</span>
-    </nav>
+  <nav>
+            <a href="../index.html"><img src="../image/favicon.ico" id="logo-img"></a>
+            
+            <div class="nav-links" id="navLinks">
+
+                <span class="icon" onclick="hidemenu()">&#10005;</span>
+                <ul>
+                    <li><a href="../index.html">Home</a></li>
+                    <li><a href="../Admission_page.php">Admission</a></li>
+                    <li><a href="../hospital_page.html">Hospital</a></li>
+                    <li><a href="../Contact_page.html">Contact</a></li>
+                    <li><a href="../index.html#course_call">Course</a></li>
+                    <li><a href="login.php">Login</a></li>
+                </ul>
+            </div> 
+            <span class="icon" onclick="showmenu()">&#9776;</span>
+        </nav> 
+  
     <form id="loginForm" method="POST">
       <div class="wrapper">
         <div class="r_form_wrap">
