e3380814b9d3fdf5dccd51ca0a7269e4a9b90e18
Shubham gangane <shubham22844@gmail.com>
2024-05-18 17:50:28 +0530

    php deleted in the admision and fees page

--- aayurvedik_collage/feespage.php
+++ aayurvedik_collage/feespage.php
@@ -104,12 +104,12 @@ $conn->close();
                   <i class="fa fa-caret-square-o-down" aria-hidden="true" id="icon"></i>
                   <select id="course" name="course" class="input" required onchange="updateTotal()">
                     <option value="select">Select the Course</option>
-                    <option value="Ayurvedic Medicine and Rasayana" <?php if ($course == 'Ayurvedic Medicine and Rasayana') echo 'selected'; ?>>Ayurvedic Medicine and Rasayana</option>
-                    <option value="Ayurvedic Nutrition and Dietetics" <?php if ($course == 'Ayurvedic Nutrition and Dietetics') echo 'selected'; ?>>Ayurvedic Nutrition and Dietetics</option>
-                    <option value="Ayurvedic Herbology and Botany" <?php if ($course == 'Ayurvedic Herbology and Botany') echo 'selected'; ?>>Ayurvedic Herbology and Botany</option>
-                    <option value="Ayurvedic Pharmacology and Formulation" <?php if ($course == 'Ayurvedic Pharmacology and Formulation') echo 'selected'; ?>>Ayurvedic Pharmacology and Formulation</option>
-                    <option value="Ayurvedic Clinical Diagnosis and Case Studies" <?php if ($course == 'Ayurvedic Clinical Diagnosis and Case Studies') echo 'selected'; ?>>Ayurvedic Clinical Diagnosis and Case Studies</option>
-                    <option value="Ayurvedic Psychology and Mind-Body Medicine" <?php if ($course == 'Ayurvedic Psychology and Mind-Body Medicine') echo 'selected'; ?>>Ayurvedic Psychology and Mind-Body Medicine</option>
+                    <option value="Ayurvedic Medicine and Rasayana" >Ayurvedic Medicine and Rasayana</option>
+                    <option value="Ayurvedic Nutrition and Dietetics" >Ayurvedic Nutrition and Dietetics</option>
+                    <option value="Ayurvedic Herbology and Botany" >Ayurvedic Herbology and Botany</option>
+                    <option value="Ayurvedic Pharmacology and Formulation" >Ayurvedic Pharmacology and Formulation</option>
+                    <option value="Ayurvedic Clinical Diagnosis and Case Studies">Ayurvedic Clinical Diagnosis and Case Studies</option>
+                    <option value="Ayurvedic Psychology and Mind-Body Medicine" >Ayurvedic Psychology and Mind-Body Medicine</option>
                   </select>
                 </div>
               </div>
