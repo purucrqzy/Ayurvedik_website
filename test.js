b07d27302d4b4171f59425901ee288ad48991d20
purucrqzy <garadpurushottam38@gmail.com>
2024-05-18 17:47:07 +0530

    modified all the test files

--- test.js
+++ test.js
@@ -1,19 +1,1 @@
 
-//scroll to top button javascript
-document.addEventListener('DOMContentLoaded',function(){
-    const scrollToTopButton= document.getElementById('scrollToTopButton');
-
-    window.addEventListener('scroll',function(){
-        if(this.window.scrollY>100){
-            scrollToTopButton.style.display='block';
-        }else{
-            scrollToTopButton.style.display='none';
-        }
-    });
-
-    scrollToTopButton.addEventListener('click',function(){
-        window.scrollTo({
-            top:0,behavior:'smooth'
-        });
-    });
-});
